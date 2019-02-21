4515d9d1e7b6b31b75bf19956557e920ec2facd4
huang.chengwei <foxmail.com>
2019-02-21 12:34:23 +0800

    fix err

--- src/main/java/com/github/jpa/spec/CriteriaImpl.java
+++ src/main/java/com/github/jpa/spec/CriteriaImpl.java
@@ -32,6 +32,12 @@ public class CriteriaImpl<T> implements Criteria<T> {
     private List<Criteria<T>> ands;
     private List<Criteria<T>> ors;
 
+    CriteriaImpl(Root<T> root,
+                        CriteriaQuery<?> query,
+                        CriteriaBuilder criteriaBuilder) {
+        this.conditions = new Conditions(root, query, criteriaBuilder, null, this);
+    }
+
     public CriteriaImpl(EntityManager em, Class<T> entityType) {
         CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
         CriteriaQuery<?> query = criteriaBuilder.createQuery();
