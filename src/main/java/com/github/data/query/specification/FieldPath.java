a943dc1a504434231c739a2b7b12455231076759
huang.chengwei <foxmail.com>
2019-02-25 17:54:54 +0800

    update

--- src/main/java/com/github/data/query/specification/FieldPath.java
+++ src/main/java/com/github/data/query/specification/FieldPath.java
@@ -5,6 +5,6 @@ import javax.persistence.criteria.Root;
 public interface FieldPath<T> {
 
     String[] getStringPaths(Root<T> root);
-//    Getters<T,?> getGetters();
     <X> javax.persistence.criteria.Path<X> getPaths(Root<T> root);
+
 }
