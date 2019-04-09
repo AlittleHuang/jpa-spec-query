573fb771772871a4b45fbbb468ecec2547ed91e2
huanghaha <huang.chengwei@foxmail.com>
2019-04-09 18:20:38 +0800

    add class AbstractQuery

---
+++ src/main/java/com/github/data/query/support/AbstractQuery.java
@@ -0,0 +1,70 @@
+package com.github.data.query.support;
+
+import com.github.data.query.specification.Expression;
+import com.github.data.query.specification.Query;
+import com.github.data.query.specification.QueryStored;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public abstract class AbstractQuery<T> extends AbstractCriteriaBuilder<T, Query<T>> implements Query<T> {
+
+    protected AbstractQueryStored<T> stored;
+
+    protected QueryStored<T> getStored() {
+        return stored;
+    }
+
+    public AbstractQuery(AbstractQueryStored<T> stored) {
+        super();
+        stored.criteria = getCriteria();
+        this.stored = stored;
+    }
+
+    protected AbstractQuery(Expression<T> expression, SimpleWhereClause<T> root, SimpleCriteria<T> criteria,
+                            AbstractQueryStored<T> stored) {
+        super(expression, root, criteria);
+        stored.criteria = criteria;
+        this.stored = stored;
+    }
+
+    @Override
+    public List<T> getResultList() {
+        return getStored().getResultList();
+    }
+
+    @Override
+    public T getSingleResult() {
+        return getStored().getSingleResult();
+    }
+
+    @Override
+    public <X> List<X> getObjectList() {
+        return getStored().getObjectList();
+    }
+
+    @Override
+    public <X> X getSingleObject() {
+        return getStored().getSingleObject();
+    }
+
+    @Override
+    public Page<T> getPage(long page, long size) {
+        return getStored().getPage(page, size);
+    }
+
+    @Override
+    public Page<T> getPage() {
+        return getStored().getPage();
+    }
+
+    @Override
+    public long count() {
+        return getStored().count();
+    }
+
+    @Override
+    public boolean exists() {
+        return getStored().exists();
+    }
+}
--- src/main/java/com/github/data/query/support/QueryImpl.java
+++ src/main/java/com/github/data/query/support/QueryImpl.java
@@ -1,18 +1,18 @@
 package com.github.data.query.support;
 
 import com.github.data.query.specification.Expression;
-import com.github.data.query.specification.Query;
-import com.github.data.query.specification.QueryStored;
-import org.springframework.data.domain.Page;
 
-import java.util.List;
+public class QueryImpl<T> extends AbstractQuery<T> {
 
-public class QueryImpl<T> extends AbstractCriteriaBuilder<T, Query<T>> implements Query<T> {
+    public QueryImpl(AbstractQueryStored<T> stored) {
+        super(stored);
+    }
 
-    protected AbstractQueryStored<T> stored;
-
-    protected QueryStored<T> getStored() {
-        return stored;
+    protected QueryImpl(Expression<T> expression,
+                        SimpleWhereClause<T> root,
+                        SimpleCriteria<T> criteria,
+                        AbstractQueryStored<T> stored) {
+        super(expression, root, criteria, stored);
     }
 
     @Override
@@ -20,56 +20,4 @@ public class QueryImpl<T> extends AbstractCriteriaBuilder<T, Query<T>> implement
         return new QueryImpl<>(expression, getWhereClause(), getCriteria(), stored);
     }
 
-    public QueryImpl(AbstractQueryStored<T> stored) {
-        super();
-        stored.criteria = getCriteria();
-        this.stored = stored;
-    }
-
-    protected QueryImpl(Expression<T> expression, SimpleWhereClause<T> root, SimpleCriteria<T> criteria,
-                        AbstractQueryStored<T> stored) {
-        super(expression, root, criteria);
-        stored.criteria = criteria;
-        this.stored = stored;
-    }
-
-    @Override
-    public List<T> getResultList() {
-        return getStored().getResultList();
-    }
-
-    @Override
-    public T getSingleResult() {
-        return getStored().getSingleResult();
-    }
-
-    @Override
-    public <X> List<X> getObjectList() {
-        return getStored().getObjectList();
-    }
-
-    @Override
-    public <X> X getSingleObject() {
-        return getStored().getSingleObject();
-    }
-
-    @Override
-    public Page<T> getPage(long page, long size) {
-        return getStored().getPage(page, size);
-    }
-
-    @Override
-    public Page<T> getPage() {
-        return getStored().getPage();
-    }
-
-    @Override
-    public long count() {
-        return getStored().count();
-    }
-
-    @Override
-    public boolean exists() {
-        return getStored().exists();
-    }
 }
